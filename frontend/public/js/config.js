a86374a345faf20034744e3e365bc567af4c37f5
LORY <lorydim44@gmail.com>
2021-08-02 10:24:13 +0200

    Exclusion config.js

--- frontend/public/js/config.js
+++
@@ -1,8 +0,0 @@
-import {getParameters} from '../js/utils/functions';
-
-export const id = getParameters();
-export const apiURL = 
-    location.hostname === 'localhost' || location.hostname === '127.0.0.1' 
-    ? 'http://localhost:3000' 
-    : 'https://api-p5-openclassroom.herokuapp.com'
-;
