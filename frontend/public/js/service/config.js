19ca8593f77b21796962ef394865777dc16116b8
LORY <lorydim44@gmail.com>
2021-08-10 17:08:39 +0200

    .gitignore config

--- frontend/public/js/service/config.js
+++
@@ -1,5 +0,0 @@
-export const apiURL = 
-    location.hostname === 'localhost' || location.hostname === '127.0.0.1' 
-    ? 'http://localhost:3000' 
-    : 'https://api-p5-openclassroom.herokuapp.com'
-;
