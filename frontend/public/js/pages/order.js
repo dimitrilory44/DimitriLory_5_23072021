c71f016e8d333007f3b65291baeda4a9cf517b81
LORY <lorydim44@gmail.com>
2021-08-12 10:44:01 +0200

    Fix cache order & Fin

--- frontend/public/js/pages/order.js
+++ frontend/public/js/pages/order.js
@@ -1,9 +1,7 @@
 import {order, total} from '../utils/variables';
-import {cacheBasket} from '../utils/functions';
 import {displayTicket} from './composants/displayOrder';
 
 (async () => {
-    cacheBasket();
-    // Je récupére les données envoyer sur localstorage
+    // Je récupére les données envoyées sur localstorage
     displayTicket(order, total);
 })();
